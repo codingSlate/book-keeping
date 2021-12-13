98692bc6072e4dee753e9a00fff1730330612d66
codingSlate <ananta.2021k@gmail.com>
2021-12-13 21:06:25 +0530

    aria live screen reader added

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -6,58 +6,67 @@ import BookList from "./BookList"
 import Form from "./Form"
 
 let recordsData = [
-    {
-      id:1,
-      bookName: 'Math Book',
-      authorName: 'Rohin',
-      description: 'Best Math books for kids',
-    },
-    {
-      id:2,
-      bookName: 'Biology Book',
-      authorName: 'Moria',
-      description: 'Introductory to Biology',
-    },
-    {
-      id:3,
-      bookName: 'Yoga',
-      authorName: 'Kalyan Mojie',
-      description: 'The healing of Body',
-    },
-    {
-      id:4,
-      bookName: 'C++',
-      authorName: 'Crafton Ford',
-      description: 'Best programming practice',
-    },
-  ];
+  {
+    id: 1,
+    bookName: 'Math Book',
+    authorName: 'Rohin',
+    description: 'Best Math books for kids',
+  },
+  {
+    id: 2,
+    bookName: 'Biology Book',
+    authorName: 'Moria',
+    description: 'Introductory to Biology',
+  },
+  {
+    id: 3,
+    bookName: 'Yoga',
+    authorName: 'Kalyan Mojie',
+    description: 'The healing of Body',
+  },
+  {
+    id: 4,
+    bookName: 'C++',
+    authorName: 'Crafton Ford',
+    description: 'Best programming practice',
+  },
+];
 
 const sortRecords = rec => rec.sort((a, b) => {
-  if(a.bookName < b.bookName){ return -1}
-  if(a.bookName > b.bookName){ return 1}
+  if (a.bookName < b.bookName) { return -1 }
+  if (a.bookName > b.bookName) { return 1 }
   return 0
 })
 
 
 const Container = () => {
-    const [allRecords, setAllRecords] = useState(sortRecords(recordsData))
-    const onAddHandler = data => {
-      setAllRecords(sortRecords([...allRecords, data]))
-    }
-    console.log("set records  ", allRecords)
-    return (
-        <Fragment>
-            <Header hLevel="1" />
-            <main>
-                <Section secHeading="Add new Book" hLevel="2">
-                    <Form onAdd={onAddHandler} />
-                </Section>
-                <Section secHeading={`${allRecords.length} Books Available`} hLevel="2">
-                    <BookList records={allRecords}/>
-                </Section>
-            </main>
-            <Footer />
-        </Fragment>
-    )
+  const [allRecords, setAllRecords] = useState(sortRecords(recordsData))
+  const [liveText, setLiveText] = useState(false)
+
+  const onAddHandler = data => {
+    setAllRecords(sortRecords([...allRecords, data]))
+    setLiveText(`${data.bookName} Successfully Added.`)
+  }
+  console.log("set records  ", allRecords)
+  return (
+    <Fragment>
+      <Header hLevel="1" />
+      <main>
+        <Section secHeading="Add new Book" hLevel="2">
+          <Form onAdd={onAddHandler} />
+        </Section>
+        <Section secHeading={`${allRecords.length} Books Available`} hLevel="2">
+          <BookList records={allRecords} />
+        </Section>
+        <div
+          className="visually-hidden"
+          aria-live="polite"
+          aria-atomic="true">
+          {liveText}
+        </div>
+      </main>
+      <Footer />
+    </Fragment>
+  )
 }
 export default Container

8fdab97a16f158ac98621c69004c98aac260680b
codingSlate <ananta.2021k@gmail.com>
2021-12-13 15:21:22 +0530

    Received input data and setForm state

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -3,13 +3,16 @@ import Header from './Header'
 import Section from "./Section"
 import Footer from "./Footer"
 import BookList from "./BookList"
+import Form from "./Form"
 
 const Container = () =>{
     return(
     <Fragment>
        <Header hLevel="1"/>
         <main>
-            <Section secHeading="Add new Book" hLevel="2"/>
+            <Section secHeading="Add new Book" hLevel="2">
+                <Form/>
+            </Section>
             <Section secHeading="Available Books" hLevel="2">
                 <BookList/>
             </Section>
---
+++ src/Components/Form.js
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+const Form = () => {
+
+    const [form, setForm] = useState('')
+    
+    const onChangeHandler = (e) => {
+        // console.log(e.target.value)
+        setForm({...form, [e.target.name]: e.target.value})
+    }
+    const onSubmitHandler = (e) => {
+        e.preventDefault()
+        console.log(form)
+    }
+    return (
+        <form onSubmit={onSubmitHandler}>
+            <label htmlFor="bookName">Book Name</label>
+            <input type="text" name="bookName" id="bookName" onChange={onChangeHandler} />
+            <label htmlFor="authorName">Book Name</label>
+            <input type="text" name="authorName" id="authorName" onChange={onChangeHandler} />
+            <label htmlFor="description">Book Name</label>
+            <textarea type="text" name="description" id="description" onChange={onChangeHandler} ></textarea>
+            <button type="submit">Add</button>
+        </form>
+    )
+}
+export default Form

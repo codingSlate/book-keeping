b694382f734a91331a7ff565f692831af9ac1a34
codingSlate <ananta.2021k@gmail.com>
2021-12-13 21:13:57 +0530

    to stop generate fluctuation of  id frequently when this component gets render we use useRef

--- src/Components/template/Input.js
+++ src/Components/template/Input.js
@@ -1,12 +1,15 @@
-import { Fragment } from 'react/cjs/react.production.min'
+import { Fragment, useRef } from 'react'
+import uniqid from 'uniqid'
 
 const Input = ({ type, labelText, ...props }) => {
+    // to stop generate id frequently when this component gets render we use useRef
+    const id = useRef(uniqid())
     return (
         <Fragment>
-            <label htmlFor={props.id}>{labelText}</label>
+            <label htmlFor={id.current}>{labelText}</label>
             {type === 'textarea' ?
-                <textarea id={props.id} {...props}></textarea> :
-                <input id={props.id} {...props} />
+                <textarea id={id.current} {...props}></textarea> :
+                <input id={id.current} {...props} />
             }
         </Fragment>
     )

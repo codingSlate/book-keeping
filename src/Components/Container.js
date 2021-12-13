9cc717e08d59aa1a0b1df131b310a8b2fa30f3f1
codingSlate <ananta.2021k@gmail.com>
2021-12-13 15:34:51 +0530

    In parent Container records state managed and updated when data received from form

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -1,28 +1,30 @@
-import { Fragment } from "react"
+import { Fragment, useState } from "react"
 import Header from './Header'
 import Section from "./Section"
 import Footer from "./Footer"
 import BookList from "./BookList"
 import Form from "./Form"
 
-const Container = () =>{
-
-    const onAddHandler =(data)=>{
-// console.log("received from from ", data)
+const Container = () => {
+    const [records, setRecords] = useState([])
+    const onAddHandler = data => {
+        // console.log("received from from ", data)
+        setRecords([...records, data])
     }
-    return(
-    <Fragment>
-       <Header hLevel="1"/>
-        <main>
-            <Section secHeading="Add new Book" hLevel="2">
-                <Form onAdd={onAddHandler}/>
-            </Section>
-            <Section secHeading="Available Books" hLevel="2">
-                <BookList/>
-            </Section>
-        </main>
-        <Footer/>
-    </Fragment>
+    // console.log("set records  ", records)
+    return (
+        <Fragment>
+            <Header hLevel="1" />
+            <main>
+                <Section secHeading="Add new Book" hLevel="2">
+                    <Form onAdd={onAddHandler} />
+                </Section>
+                <Section secHeading="Available Books" hLevel="2">
+                    <BookList />
+                </Section>
+            </main>
+            <Footer />
+        </Fragment>
     )
 }
 export default Container
